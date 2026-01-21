f34230a5ee0acee2140f1fffb0b3019768f15723
Jonathan Granda <jhonatangranda.2002@gmail.com>
2026-01-21 15:58:19 -0500

    add folders y config

---
+++ src/main/java/com/pragma/challenge/msvc_plaza/MsvcPlazaApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.challenge.msvc_plaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPlazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/challenge/msvc_plaza/MsvcPlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.challenge.msvc_plaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
